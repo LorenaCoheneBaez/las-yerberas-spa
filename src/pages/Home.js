06ce3199ca30f692ad516643aac6055900db015d
LorenaCoheneBaez <loreley.cb15@gmail.com>
2022-01-01 13:06:08 -0300

    add color palette and font

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home DM Sans</h1>
+      <p>lorem ipsum lorem prem sdjhsdfnmvgfgdfg.</p>
+    </div>
+  )
+}
