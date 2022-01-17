f0797a73912c41656ad816a8a0e036956c8ae31c
LorenaCoheneBaez <loreley.cb15@gmail.com>
2022-01-17 14:52:30 -0300

    Add About me structure

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      
+      <aside>
+       
+      </aside>
+      <h2>¡Hola!</h2>
+    <p>Soy Magalia </p>
     </div>
   )
 }
