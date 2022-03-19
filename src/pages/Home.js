02b5c736766f365539b0738680867a1292e1f396
LorenaCoheneBaez <loreley.cb15@gmail.com>
2022-03-19 00:30:43 -0300

    add first styles to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {benefits} from '../data/benefits'
 
+const subtitulo = ["BIENESTAR", "SALUD", "RELAJACIÓN"]
 
 export default function Home() {
   return (
-    <div>
+    <div className='home'>
       <h1>Bienvenid@s a Masoterapia Las Yerberas</h1>
-      <p>SOY EL HOME.</p>
-      <img className='border border-danger banner-home' src='../../assets/banner-masaje.jpg' alt='banner-masaje'/>
+      <section>
+        <div id='banner'>
+          {subtitulo.map((element, i) =>{
+            return <h2 key={i+1} className='focus-in-expand'>{element}</h2>
+          }
+          )}
+        </div>
+      </section>
+      <section>
+        <h3>Beneficios de la masoterapia:</h3>
+        <ul class="list-group">
+          {benefits.map((element, i)=>{
+            return <li key={i} id='list-group-item' className="list-group-item">{element}</li>
+          }
+          )}
+        </ul>
+      </section>
     </div>
   )
 }
