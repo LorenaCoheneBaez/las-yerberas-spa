2b6d7bc48de598a5e5e4b0708ee33e354fd541f6
LorenaCoheneBaez <loreley.cb15@gmail.com>
2022-03-24 00:55:54 -0300

    add structure about

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,22 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      <aside>
-       
-      </aside>
-      <h2>¡Hola!</h2>
-    <p>Soy Magalia </p>
+      <h2>Acerca de mí</h2>
+      <div className="card mb-3" >
+        <div className="row g-0">
+            <div className="col-md-4">
+              <img src="../../assets/aceites.jpg" className="img-fluid rounded-start" alt="Imagen acerca de mí"/>
+            </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h3 className="card-title">¡Hola, soy Magalía!</h3>
+              <p className="card-text">Estudié masoterapia en la escuela de Alta Formación Profesional (ACP), me dedico a ayudarte a encontrar salud, bienestar y calidad de vida.</p>
+              <p className="card-text">Porque considero que todos necesitamos sentirnos mejor, y darnos un momento para nosotros mismos.</p>
+              <p className="card-text">Te invito a que me escribas y te regales ese instante de paz que mereces.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
